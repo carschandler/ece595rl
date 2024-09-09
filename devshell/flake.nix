027bc9e5597cfd152665ae456e67e8323024e204
carschandler <92899389+carschandler@users.noreply.github.com>
2024-09-09 14:40:15 -0500

    Almost done with HW1

--- devshell/flake.nix
+++ devshell/flake.nix
@@ -8,15 +8,20 @@
   outputs =
     { nixpkgs, ... }:
     let
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
     in
     {
-      devShells.aarch64-darwin.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.quarto
-        ];
-      };
-
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.quarto
+          ];
+        };
+      });
     };
 }
