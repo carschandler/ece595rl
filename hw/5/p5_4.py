37139774b786dbebc1e5ade8805515c483a46528
carschandler <92899389+carschandler@users.noreply.github.com>
2024-12-01 22:44:14 -0600

    Correction to p2, p5

--- hw/5/p5_4.py
+++ hw/5/p5_4.py
@@ -1,19 +1,6 @@
 import numpy as np
-from p5_1 import pref_1_over_2, pref_1_over_3, r1, r2, r3
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
+from p5_1 import pref_1_over_2, pref_1_over_3
 
 pref_3_over_1 = 1 - pref_1_over_3
 
-mle_loss = -(
-    pref_1_over_2 * np.log(sigmoid(r1 - r2))
-    + pref_3_over_1 * np.log(sigmoid(r3 - r1))
-)
-
-mle_loss_2 = -(
-    (1 - pref_1_over_2) * np.log(sigmoid(r2 - r1))
-    + pref_3_over_1 * np.log(sigmoid(r3 - r1))
-)
+mle_loss = -(np.log(pref_1_over_2) + np.log(pref_3_over_1))
